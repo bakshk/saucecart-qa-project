4ce10c41f5b7142dd5949daafcdb8ff593744829
bakshk <bakshikavya143@gmail.com>
2025-04-16 21:23:36 -0500

    Create login_test.py

---
+++ Automation/Automation/login_test.py
@@ -0,0 +1,18 @@
+# login_test.py – SauceCart QA Project
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+# Enter credentials
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+
+# Assert login success
+assert "inventory" in driver.current_url
+
+print("Login test passed.")
+driver.quit()
